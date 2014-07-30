c2371f7a28d8de7e5b30908d6556adda76a479da
atupal <atupalykl@gmail.com>
2014-07-30 00:54:25 +0000

    add 2482

--- poj/2482_Stars_in_Your_Window_线段树.cpp
+++ poj/2482_Stars_in_Your_Window_线段树.cpp
@@ -1,4 +1,11 @@
-/**/
+/*
+   对每个星星来说，左上角在以这个星星为右下顶点的矩形内部的矩形都可以围住这个星星。
+   note：
+        1. 离散化时为闭区间（a，b），因为边界不算。
+        2. mask 是累加的
+        3. 高度相等时，出边应该排在前面，同1
+   计算最大的重叠的矩形的值的和即可。
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,11 +17,13 @@ using namespace std;
 #define rson (root<<1|1)
 #define max(a, b) ((a)>(b)?(a):(b))
 
-const int maxn = 100100;
-int n, w, h;
-int X[maxn];
+typedef long long lint;
 
-typedef unsigned int lint;
+const int maxn = 100100;
+int n;
+lint w, h;
+lint X[maxn<<1];
+
 
 static struct {
   lint left;
@@ -22,27 +31,27 @@ static struct {
   int cover;
   int mask;
   int max;
-} tree[maxn*3+1];
+} tree[maxn*3];
 
 struct line {
   lint left;
   lint right;
-  int hight;
-  int val;
+  lint hight;
+  lint val;
 } lines[maxn<<1];
 
-void build(int root, lint left, lint right) {
+void build(int root, int left, int right) {
   tree[root].left = X[left];
   tree[root].right = X[right];
   tree[root].cover = 0;
   tree[root].mask = 0;
   tree[root].max = 0;
-  if (left == right) {
+  if (left >= right-1) {
     return;
   }
   lint mid = (left + right) >> 1;
   build(lson, left, mid);
-  build(rson, mid+1, right);
+  build(rson, mid, right);
 }
 
 void push_down(int root) {
@@ -51,8 +60,8 @@ void push_down(int root) {
     tree[rson].cover += tree[root].mask;
     tree[lson].max += tree[root].mask;
     tree[rson].max += tree[root].mask;
-    tree[lson].mask = tree[root].mask;
-    tree[rson].mask = tree[root].mask;
+    tree[lson].mask += tree[root].mask;
+    tree[rson].mask += tree[root].mask;
     tree[root].mask = 0;
   }
 }
@@ -65,9 +74,9 @@ void insert(int root, lint left, lint right, int c) {
   if (left <= tree[root].left and tree[root].right <= right) {
     tree[root].cover += c;
     tree[root].max += c;
-    tree[root].mask = c;
+    tree[root].mask += c;
     return;
-  } else if (left > tree[root].right or right < tree[root].left) {
+  } else if (left >= tree[root].right or right <= tree[root].left) {
     return;
   }
   push_down(root);
@@ -77,23 +86,23 @@ void insert(int root, lint left, lint right, int c) {
 }
 
 int cmp(const line a, const line b) {
-  return a.hight < b.hight or (a.hight == b.hight and a.val > 0);
+  return a.hight < b.hight or (a.hight == b.hight and a.val < 0);
 }
 
 void solve() {
   int ans = 0;
   int i = 0, j = 0, t;
-  sort(X, X+n);
-  while (j < n) {
+  int _2n = n << 1;
+  sort(X, X+_2n);
+  while (j < _2n) {
     t = X[j];
-    while (j+1 < n and X[j+1] == t) ++ j;
+    while (j+1 < _2n and X[j+1] == t) ++ j;
     X[i] = t;
     ++ i;
     ++ j;
   }
   build(1, 0, i-1);
-  sort(lines, lines+(n<<1), cmp);
-  int _2n = n << 1;
+  sort(lines, lines+_2n, cmp);
   for (int i = 0; i < _2n; ++ i) {
     insert(1, lines[i].left, lines[i].right, lines[i].val);
     ans = max(ans, tree[1].max);
@@ -102,20 +111,24 @@ void solve() {
 }
 
 int input() {
-  if (scanf("%d %d %d", &n, &w, &h) == EOF) return 0;
-  int x, y, c;
+  if (scanf("%d %lld %lld", &n, &w, &h) == EOF) return 0;
+  lint x, y;
+  int c;
   for (int i = 0; i < n; ++ i) {
-    scanf("%d %d %d", &x, &y, &c);
-    X[i] = x;
-    lines[i<<1].left = max(0, x - w + 1);
-    lines[i<<1].right = max(0, x + w - 1);
-    lines[i<<1].hight = max(0, y - h + 1);
+    scanf("%lld %lld %d", &x, &y, &c);
+    X[i<<1] = x;
+    X[i<<1|1] = x - w;
+    lines[i<<1].left = x - w;
+    lines[i<<1].right = x;
+    lines[i<<1].hight = y;
     lines[i<<1].val = c;
 
-    lines[i<<1|1].left = max(0, x - w + 1);
-    lines[i<<1|1].right = max(0, x + w - 1);
-    lines[i<<1|1].hight = max(0, y + h - 1);
+    lines[i<<1|1].left = x - w;
+    lines[i<<1|1].right = x;
+    lines[i<<1|1].hight = y + h;
     lines[i<<1|1].val = -c;
+
+    //printf("(%d, %d): %d - %d\n", x, y, lines[i<<1].left, lines[i<<1].right);
   }
   return 1;
 }
