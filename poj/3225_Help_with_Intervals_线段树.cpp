67905897a09970891327886f5357a633e76eb208
atupal <atupalykl@gmail.com>
2014-08-20 08:46:44 +0000

    update 3225

--- poj/3225_Help_with_Intervals_线段树.cpp
+++ poj/3225_Help_with_Intervals_线段树.cpp
@@ -16,6 +16,7 @@ static struct {
     int lbd;
     int cover;
     int mask;
+    int flag;
 } tree[maxn*3+11];
 
 void build(int root, int left, int right) {
@@ -24,6 +25,7 @@ void build(int root, int left, int right) {
     tree[root].lbd = 0;
     tree[root].cover = 0;
     tree[root].mask = 0;
+    tree[root].flag = 0;
     if (left == right-1) {
         return;
     }
@@ -44,10 +46,14 @@ void build(int root, int left, int right) {
 void push_down(int root) {
     int c = tree[root].mask;
     if (c) {
-      tree[lson].cover = tree[root].cover;
-      tree[rson].cover = tree[root].cover;
-      tree[lson].lbd = tree[root].lbd;
-      tree[rson].lbd = tree[root].cover;
+      //tree[lson].cover = tree[root].cover;
+      //tree[rson].cover = tree[root].cover;
+      //tree[lson].lbd = tree[root].lbd;
+      //tree[rson].lbd = tree[root].cover;
+      line(tree[lson].cover);
+      line(tree[rson].cover);
+      line(tree[lson].lbd);
+      line(tree[rson].lbd);
       tree[lson].mask = c;
       tree[rson].mask = c;
       tree[root].mask = 0;
@@ -55,6 +61,13 @@ void push_down(int root) {
 }
 
 void update(int root) {
+  if (tree[lson].cover == tree[rson].cover 
+      and tree[lson].cover == tree[rson].lbd) {
+    tree[root].flag = 1;
+    tree[root].cover = tree[lson].cover;
+  } else {
+    tree[root].flag = 0;
+  }
 }
 
 void insert(int root, int left, int right, int l, int r, int c) {
@@ -68,12 +81,16 @@ void insert(int root, int left, int right, int l, int r, int c) {
 
   if (left <= tree[root].left and tree[root].right <= right) {
     line(tree[root].cover);
+      if (left == 2 and right == 4) {
+        //printf("debug:<%d, %d>\n", tree[root].left, tree[root].right);
+      }
     if (left != tree[root].left) {
       line(tree[root].lbd);
     }
     tree[root].mask = c;
-    if (flag) {
-      return;
+    tree[root].flag = 1;
+    if (not flag or 1) {
+      //return;
     }
   }
   if (left >= tree[root].right or right < tree[root].left or (right == tree[root].left and not r)) {
@@ -85,25 +102,33 @@ void insert(int root, int left, int right, int l, int r, int c) {
   push_down(root);
   insert(lson, left, right, l, r, c);
   insert(rson, left, right, l, r, c);
-  update(root);
+  //update(root);
 }
 
+static struct {
+  int left;
+  int cover;
+} ans[maxn+3];
+
 void query(int root, int left, int right) {
-  if (left >= tree[root].right or right <= tree[root].left ) {
-    return;
-  }
-  push_down(root);
   if (tree[root].left+1 == tree[root].right) {
     if (tree[root].cover) {
       if (tree[root].lbd) {
-        printf("[%d,%d)", tree[root].left, tree[root].right);
+        //printf("[%d,%d)", tree[root].left, tree[root].right);
+        ans[tree[root].left].cover = 1;
+        ans[tree[root].left].left = 1;
       } else {
-        printf("(%d,%d)", tree[root].left, tree[root].right);
+        //printf("(%d,%d)", tree[root].left, tree[root].right);
+        ans[tree[root].left].cover = 1;
+        ans[tree[root].left].left = 0;
       }
     } else if (tree[root].lbd) {
-      printf("[%d,%d] ", tree[root].left, tree[root].left);
+      //printf("[%d,%d] ", tree[root].left, tree[root].left);
+      ans[tree[root].left].cover = 0;
+      ans[tree[root].left].left = 1;
     }
   } else {
+    push_down(root);
     query(lson, left, right);
     query(rson, left, right);
   }
@@ -114,15 +139,15 @@ void U(int root, int left, int right, int a, int b) {
   insert(root, left, right, a, b, 1);
 }
 void I(int root, int left, int right, int a, int b) {
-  insert(root, 1, left, 1, not b, -1);
-  insert(root, left, maxn, not a, 1, -1);
+  insert(root, 0, left, 1, not a, -1);
+  insert(root, right, maxn, not b, 1, -1);
 }
 void D(int root, int left, int right, int a, int b) {
   insert(root, left, right, a, b, -1);
 }
 void C(int root, int left, int right, int a, int b) {
-  insert(root, 1, left, 1, not b, -1);
-  insert(root, left, maxn, not a, 1, -1);
+  insert(root, 0, left, 1, not a, -1);
+  insert(root, right, maxn, not b, 1, -1);
   insert(root, left, right, a, b, 2);
 }
 void S(int root, int left, int right, int a, int b) {
@@ -135,16 +160,40 @@ void solve() {
     int a, b;
     build(1, 0, maxn+1);
     while (scanf("%c %c%d,%d%c\n", &order, &l, &a, &b, &r) != EOF) {
-        //int left = 0, right = 0;
+        //printf("%c %c%d,%d%c\n", order, l, a,b,r);
         f[(int)order](1, a, b, l=='[', r==']');
-        printf("%c %c%d,%d%c\n", order, l, a,b,r);
-        query(1, 0, 10);
-        printf("\n");
+        //query(1, 0, 10);
+        //printf("\n");
     }
-    //query(1, 0, 10);
+    query(1, 0, maxn);
+    //printf("\n");
 }
 
 int main() {
     solve();
+    int l = 0;
+    while (l<maxn+3) {
+      if (ans[l].left) {
+        printf("[%d,", l);
+      } else if (ans[l].cover) {
+        printf("(%d,", l);
+      } else {
+        ++ l;
+        continue;
+      }
+
+      while (l<maxn+3) {
+        if (not ans[l].cover) {
+          if (ans[l].left) {
+            printf("%d] ", l);
+          } else {
+            printf("%d) ", l);
+          }
+          ++ l;
+          break;
+        }
+        ++ l;
+      }
+    }
     return 0;
 }
