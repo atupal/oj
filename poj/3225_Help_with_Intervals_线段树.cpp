43d43ab945a88b6c7dc79d88627e9f126598aa56
atupal <atupalykl@gmail.com>
2014-08-25 09:00:44 +0000

    add submit.py: auto submit problem to poj and display the result

--- poj/3225_Help_with_Intervals_线段树.cpp
+++ poj/3225_Help_with_Intervals_线段树.cpp
@@ -8,30 +8,32 @@
 #define lson root<<1
 #define rson root<<1|1
 
-const int maxn = 65535;
+const int maxn = (65535<<1) + 3;
 
 static struct {
     int left;
     int right;
     int lbd;
+    int rbd;
     int cover;
     int mask;
     int flag;
-} tree[maxn*3+11];
+} tree[maxn*4+11];
 
 void build(int root, int left, int right) {
     tree[root].left = left;
     tree[root].right = right;
-    tree[root].lbd = 0;
     tree[root].cover = 0;
     tree[root].mask = 0;
-    tree[root].flag = 0;
-    if (left == right-1) {
-        return;
+    tree[root].flag = 1;
+    tree[root].lbd = left-1;
+    tree[root].rbd = right+1;
+    if (left >= right) {
+      return;
     }
     int mid = (left + right) >> 1;
     build(lson, left, mid);
-    build(rson, mid, right);
+    build(rson, mid+1, right);
 }
 
 #define line(x)\
@@ -44,156 +46,114 @@ void build(int root, int left, int right) {
   }\
 
 void push_down(int root) {
-    int c = tree[root].mask;
-    if (c) {
-      //tree[lson].cover = tree[root].cover;
-      //tree[rson].cover = tree[root].cover;
-      //tree[lson].lbd = tree[root].lbd;
-      //tree[rson].lbd = tree[root].cover;
-      line(tree[lson].cover);
-      line(tree[rson].cover);
-      line(tree[lson].lbd);
-      line(tree[rson].lbd);
-      tree[lson].mask = c;
-      tree[rson].mask = c;
-      tree[root].mask = 0;
+  int c = tree[root].mask;
+  if (c) {
+    tree[lson].cover = tree[root].cover;
+    tree[rson].cover = tree[root].cover;
+    tree[lson].mask = c;
+    tree[rson].mask = c;
+    if (tree[root].cover) {
+      tree[lson].lbd = tree[lson].right;
+      tree[lson].rbd = tree[lson].left;
+      tree[rson].lbd = tree[rson].right;
+      tree[rson].rbd = tree[rson].left;
+    } else {
+      tree[lson].lbd = -1; 
+      tree[lson].rbd = -1; 
+      tree[rson].lbd = -1; 
+      tree[rson].rbd = -1; 
     }
+    tree[lson].flag = 1;
+    tree[rson].flag = 1;
+    tree[root].mask = 0;
+  }
 }
 
 void update(int root) {
-  if (tree[lson].cover == tree[rson].cover 
-      and tree[lson].cover == tree[rson].lbd) {
-    tree[root].flag = 1;
+  if (tree[lson].cover == tree[rson].cover and tree[lson].flag and tree[rson].flag) {
     tree[root].cover = tree[lson].cover;
+    tree[root].flag = 1;
   } else {
     tree[root].flag = 0;
   }
+
+  tree[root].lbd = tree[lson].lbd;
+  if (tree[rson].lbd != -1 and tree[lson].lbd == tree[lson].right) {
+    tree[root].lbd = tree[rson].lbd;
+  }
+  tree[root].rbd = tree[rson].rbd;
+  if (tree[lson].rbd != -1 and tree[rson].rbd == tree[rson].left) {
+    tree[root].rbd = tree[lson].rbd;
+  }
 }
 
-void insert(int root, int left, int right, int l, int r, int c) {
-  int flag = 0;
-  if ( (l and left == tree[root].left and left != right)
-        or (r and right == tree[root].left and left != right)
-        or (l and r and left == right and left == tree[root].left)) {
-    line(tree[root].lbd);
-    flag = 1;
-  }
-
+void insert(int root, int left, int right, int c) {
+  tree[root].flag = 0;
   if (left <= tree[root].left and tree[root].right <= right) {
     line(tree[root].cover);
-      if (left == 2 and right == 4) {
-        //printf("debug:<%d, %d>\n", tree[root].left, tree[root].right);
-      }
-    if (left != tree[root].left) {
-      line(tree[root].lbd);
-    }
     tree[root].mask = c;
     tree[root].flag = 1;
-    if (not flag or 1) {
-      //return;
+    if (tree[root].cover) {
+      tree[root].lbd = tree[root].right;
+      tree[root].rbd = tree[root].left;
+    } else {
+      tree[root].lbd = -1;
+      tree[root].rbd = -1;
     }
-  }
-  if (left >= tree[root].right or right < tree[root].left or (right == tree[root].left and not r)) {
+    return;
+  } else if (left > tree[root].right or right < tree[root].left) {
     return;
   } 
-  if (tree[root].left+1 == tree[root].right) {
+  push_down(root);
+  insert(lson, left, right, c);
+  insert(rson, left, right, c);
+  update(root);
+}
+
+void (*f[256]) (int root, int left, int right);
+void U(int root, int left, int right) {
+  insert(root, left, right, 1);
+}
+void I(int root, int left, int right) {
+  insert(root, -1, left-1, -1);
+  insert(root, right+1, maxn+1, -1);
+}
+void D(int root, int left, int right) {
+  insert(root, left, right, -1);
+}
+void C(int root, int left, int right) {
+  insert(root, -1, left-1, -1);
+  insert(root, right+1, maxn+1, -1);
+  insert(root, left, right, 2);
+}
+void S(int root, int left, int right) {
+  insert(root, left, right, 2);
+}
+
+void query(int root) {
+  if (tree[root].left == tree[root].right) {
     return;
   }
-  push_down(root);
-  insert(lson, left, right, l, r, c);
-  insert(rson, left, right, l, r, c);
-  //update(root);
-}
+  query(lson);
 
-static struct {
-  int left;
-  int cover;
-} ans[maxn+3];
+  l = tree[lson].rbd;
 
-void query(int root, int left, int right) {
-  if (tree[root].left+1 == tree[root].right) {
-    if (tree[root].cover) {
-      if (tree[root].lbd) {
-        //printf("[%d,%d)", tree[root].left, tree[root].right);
-        ans[tree[root].left].cover = 1;
-        ans[tree[root].left].left = 1;
-      } else {
-        //printf("(%d,%d)", tree[root].left, tree[root].right);
-        ans[tree[root].left].cover = 1;
-        ans[tree[root].left].left = 0;
-      }
-    } else if (tree[root].lbd) {
-      //printf("[%d,%d] ", tree[root].left, tree[root].left);
-      ans[tree[root].left].cover = 0;
-      ans[tree[root].left].left = 1;
-    }
-  } else {
-    push_down(root);
-    query(lson, left, right);
-    query(rson, left, right);
-  }
-}
-
-void (*f[256]) (int root, int left, int right, int a, int b);
-void U(int root, int left, int right, int a, int b) {
-  insert(root, left, right, a, b, 1);
-}
-void I(int root, int left, int right, int a, int b) {
-  insert(root, 0, left, 1, not a, -1);
-  insert(root, right, maxn, not b, 1, -1);
-}
-void D(int root, int left, int right, int a, int b) {
-  insert(root, left, right, a, b, -1);
-}
-void C(int root, int left, int right, int a, int b) {
-  insert(root, 0, left, 1, not a, -1);
-  insert(root, right, maxn, not b, 1, -1);
-  insert(root, left, right, a, b, 2);
-}
-void S(int root, int left, int right, int a, int b) {
-  insert(root, left, right, a, b, 2);
+  query(rson);
 }
 
 void solve() {
     f['U'] = U, f['I'] = I, f['D'] = D, f['C'] = C, f['S'] = S;
     char order, l, r;
     int a, b;
-    build(1, 0, maxn+1);
+    build(1, -1, maxn+1);
     while (scanf("%c %c%d,%d%c\n", &order, &l, &a, &b, &r) != EOF) {
         //printf("%c %c%d,%d%c\n", order, l, a,b,r);
-        f[(int)order](1, a, b, l=='[', r==']');
-        //query(1, 0, 10);
-        //printf("\n");
+        f[(int)order](1, (a<<1)+(l=='('), (b<<1)-(r==')'));
     }
-    query(1, 0, maxn);
-    //printf("\n");
+    query(1);
 }
 
 int main() {
     solve();
-    int l = 0;
-    while (l<maxn+3) {
-      if (ans[l].left) {
-        printf("[%d,", l);
-      } else if (ans[l].cover) {
-        printf("(%d,", l);
-      } else {
-        ++ l;
-        continue;
-      }
-
-      while (l<maxn+3) {
-        if (not ans[l].cover) {
-          if (ans[l].left) {
-            printf("%d] ", l);
-          } else {
-            printf("%d) ", l);
-          }
-          ++ l;
-          break;
-        }
-        ++ l;
-      }
-    }
     return 0;
 }
---
+++ poj/submit.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+
+import requests
+
+s = requests.Session()
+
+user = 'atupal'
+password = ''
+
+data = {
+    'user_id1': user,
+    'password1': password,
+    'B1': 'login',
+    'url': '/',
+    }
+
+r = s.post('http://poj.org/login', data=data, allow_redirects=0)
+
+
+proxy_password = ''
+http_proxy = {
+    'http': 'http://atupal:%s@j10.jayproxy.org:3000' % proxy_password
+    }
+
+import sys
+
+with open(sys.argv[1]) as fi:
+  data = {
+      'problem_id': sys.argv[1].split('_')[0],
+      'language': '4',
+      'source': fi.read(),
+      'submit': 'Submit'
+  }
+
+s.post('http://poj.org/submit', proxies=http_proxy, data=data)
+
+from bs4 import BeautifulSoup
+import os
+import time
+
+def colorful_print(text, color='red'):
+  color_dict = {
+      'red': '\033[31m',
+      }
+  print color_dict['color']+text+'\033[0m'
+
+while 1:
+  try:
+    r = s.get('http://poj.org/status')
+    soup = BeautifulSoup(r.text)
+    os.system('cls')
+    for tr in soup('tr')[1:13]:
+      for td in tr('td'):
+        print td.text,
+      print
+    print '-' * 100
+    time.sleep(1)
+  except KeyboardInterrupt:
+    exit(0)
+
