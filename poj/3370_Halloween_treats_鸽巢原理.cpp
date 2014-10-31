4fa6ef03d3582677dc120d9c094837308c9f8a76
atupal <atupalykl@gmail.com>
2014-10-31 12:28:56 +0000

    终于 100 题。。。跪了 （逃

---
+++ poj/3370_Halloween_treats_鸽巢原理.cpp
@@ -0,0 +1,52 @@
+/*
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+const int maxn = 100010;
+int c, n;
+int s[maxn];
+int pos[maxn];
+
+void solve () {
+  //scanf("%d", &n);
+  for (int i = 0; i < n; ++ i) {
+    scanf("%d", s+i);
+  }
+  memset(pos, -1, sizeof(pos));
+  int sum = 0;;
+  for (int i = 0; i < n; ++ i) {
+    sum += s[i] % c;
+    sum %= c;
+    if (sum == 0) {
+      for (int x = 0; x < i; ++ x) {
+        printf("%d ", x+1);
+      }
+      printf("%d\n", i+1);
+      return;
+    } else {
+      if (pos[sum] == -1) pos[sum] = i;
+      else {
+        for (int x = pos[sum]+1; x < i; ++ x) {
+          printf("%d ", x+1);
+        }
+        printf("%d\n", i+1);
+        return;
+      }
+    }
+  }
+}
+
+int main() {
+  while (1) {
+    scanf("%d %d", &c, &n);
+    if (n + c == 0) break;
+    solve();
+  }
+
+  return 0;
+}
